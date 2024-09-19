88b9cc84b5010102d5f2533328ec5045efc6fd3e
mslade50 <107962250+mslade50@users.noreply.github.com>
2024-09-19 15:42:37 -0400

    Update nfl_weather.py

--- pages/nfl_weather.py
+++ pages/nfl_weather.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from datetime import datetime
 from streamlit_plotly_events import plotly_events
 
 st.set_page_config(layout="wide")
